01f21888beb56df49c2b99151a3215a82b28e61f
kvirulent <xirulent@gmail.com>
2025-08-30 15:12:23 -0500

    add workflows, change structure

---
+++ .github/scripts/gdi.py
@@ -0,0 +1,16 @@
+import os, json, datetime
+
+ignore = {".git", ".github", "__pycache__"}
+
+modules = []
+for item in os.listdir("./modules"):
+    if os.path.isdir(item) and item not in ignore:
+        modules.append({"name": item, "path": f"modules/{item}/"})
+
+legend = {
+    "last_updated": datetime.datetime.utcnow().isoformat() + "Z",
+    "modules": modules
+}
+
+with open("legend.json", "w") as f:
+    json.dump(legend, f, indent=2)
